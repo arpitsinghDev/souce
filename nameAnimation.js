b47bc42a11f890315fec25167719999f6d31129c
arpit singh <45120042+arpitsinghDev@users.noreply.github.com>
2018-12-02 02:29:29 +0530

    Add files via upload

---
+++ nameAnimation.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	var $randomnbr=$('.nbr');
+	var $timer =30;
+	var $it;
+	var $index;
+	var $data=0;
+	var change;
+	var letters=["a","r","p","i","t"];
+	$randomnbr.each(function(){
+		change =Math.round(Math.random()*100);
+		$(this).attr('data-change',change);
+	});
+	function random(){
+		return Math.round(Math.random()*9);
+	};
+	function select(){
+		return Math.round(Math.random()* $randomnbr.length+1);
+	}
+	function value(){
+		$('.nbr:nth-child('+select()+')').html(''+random()+'');
+		$('.nbr:nth-child('+select()+')').attr('data-number',$data);
+		$data++
+		$randomnbr.each(function(){
+			if(parseInt($(this).attr('data-number'))>parseInt($(this).attr('data-change'))){
+				index=$('.ltr').index(this);
+				$(this).html(letters[index]);
+				$(this).removeClass('nbr');
+			}
+		})
+	}
+	$it=setInterval(value,$timer);
+})

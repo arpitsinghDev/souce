e1e02694519aed53449f9c2658149646fe2dec26
arpit singh <45120042+arpitsinghDev@users.noreply.github.com>
2018-12-10 16:40:31 +0530

    Add files via upload

--- ridhima/script.js
+++ ridhima/script.js
@@ -1,51 +1,51 @@
 var qAnda=[
 {
-	qusetion:"green",
+	question:"green",
 	answer:'#0f0',
 	audiof:"audio/green.mp3"
 },
 {
-	qusetion:"red",
+	question:"red",
 	answer:'#f00',
 	audiof:"audio/red.mp3"
 },
 {
-	qusetion:"blue",
+	question:"blue",
 	answer:'#00f',
 	audiof:"audio/blue.mp3"
 },
 {
-	qusetion:"white",
+	question:"white",
 	answer:'#fff',
 	audiof:"audio/white.mp3"
 },
 {
-	qusetion:"black",
+	question:"black",
 	answer:'#000',
 	audiof:"audio/black.mp3"
 },
 {
-	qusetion:"grey",
+	question:"grey",
 	answer:'#555',
 	audiof:"audio/grey.mp3"
 },
 {
-	qusetion:"yellow",
+	question:"yellow",
 	answer:'#ff0',
 	audiof:"audio/yellow.mp3"
 },
 {
-	qusetion:"pink",
+	question:"pink",
 	answer:'#f0f',
 	audiof:"audio/pink.mp3"
 },
 {
-	qusetion:"Purple",
+	question:"Purple",
 	answer:'#505',
 	audiof:"audio/purple.mp3"
 },
 {
-	qusetion:"violet",
+	question:"violet",
 	answer:'#e8e',
 	audiof:"audio/violet.mp3"
 }
@@ -53,16 +53,19 @@ var qAnda=[
 function sound(src) {
     this.sound = document.createElement("audio");
     this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("preload", "autoplay");
+    this.sound.setAttribute("id", "audio");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
+   
     document.body.appendChild(this.sound);
     this.play = function(){
-        this.sound.play();
+        this.sound.play()
     }
     this.stop = function(){
         this.sound.pause();
     }
+
 }
 var question=document.getElementById("question")
 var option1=document.getElementById("option1")
@@ -79,23 +82,16 @@ function creater(a){
 		m=[a,a+1,a+2,a+3];}
 	m.sort(()=> .5-Math.random());
 let value=""
-value=`Where is color ${qAnda[a].qusetion}`;
-var mysound=new sound("audio/wsc.mp3")
-
-document.body.addEventListener("ended",function(){
-mysound.play()
-setTimeout(function(){colorName.play()},1000)
-},true)
-
-window.onload = function(){
-alert("")
-mysound.play()
-setTimeout(function(){colorName.play()},1000)
-}
-
+value=`Where is color ${qAnda[a].question}`;
 
 let colorName = new sound(qAnda[a].audiof)
-
+//////////////////
+question.addEventListener("click",()=>{
+	let mysound=new sound("audio/wsc.mp3")
+	mysound.play()
+	setTimeout(()=>colorName.play(),890)
+})
+///////////////////////////////
 question.innerHTML=value
 let oc1=option1.style.backgroundColor=qAnda[m[0]].answer
 let oc2=option2.style.backgroundColor=qAnda[m[1]].answer
